ac0e5b95064a909ce4ce59874099c174b3773682
Natasha Whitney <natiwhitney@gmail.com>
2016-12-08 09:57:17 -0800

    Reformulate line split with recursions

--- advent-of-code-scala/src/main/scala-2.12/daySeven.scala
+++ advent-of-code-scala/src/main/scala-2.12/daySeven.scala
@@ -5,10 +5,44 @@ import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 object daySeven extends App {
 
   def isAbba(abba:String): Boolean = {
-    return (abba(0) == abba(3)) && (abba(0) != abba(1)) && (abba(1) == abba(2))
+    return (abba.length <= 4) && (abba(0) != abba(1)) && (abba(0) == abba(3))  && (abba(1) == abba(2))
   }
 
-  
+  def isAbbaLongString(longString:String): Boolean = {
+    val abbas = longString.sliding(4,1).toList.filter(isAbba)
+    return abbas.length > 0
+  }
 
+  val fileName = "daySevenInput.csv"
+  def parseInputs(fileName:String): List[List[String]] = {
+    val bufferedSource = io.Source.fromFile(fileName)
+    val parsedInputs = new ListBuffer[List[String]]()
+
+    for (line <- bufferedSource.getLines()) {
+      val parsedLine = parseLine(line)
+      parsedInputs += parsedLine
+    }
+    return parsedInputs.toList
+  }
+
+  // multiple square brackets
+  def parseLine(line:String): List[String] = {
+    val lineSplits = line.split('[' )
+    val lineSplits2 = lineSplits(1).split(']')
+    return List(lineSplits(0),lineSplits2(0),lineSplits2(1))
+  }
+
+  def validateInput(input:List[String]): Int = {
+   if ((isAbbaLongString(input(0)) || isAbbaLongString(input(2))) && !isAbbaLongString(input(1))) {
+     return 1
+   } else {
+     return 0
+   }
+  }
+
+  val parsedInputs = daySeven.parseInputs("daySevenInputs.csv")
+  val validInputs = parsedInputs.map(validateInput)
+  val total = validInputs.reduceLeft(_+_)
+  println(s"total:$total")
 }
 
